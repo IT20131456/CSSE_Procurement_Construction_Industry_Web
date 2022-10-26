4ccc2286463c428058e10e7aba30c75067868d4a
IT20128036 <87744181+IT20128036@users.noreply.github.com>
2022-10-27 00:48:39 +0530

    supplier login ,register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sample from "../src/components/sample"
+import SupplierLogin from "./components/IT20128036/supplier/UserLogin"
+import SupplierRegistration from"./components/IT20128036/supplier/SupplierRegistration"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Sample/>} />
+        <Route path="/user/login" element={<SupplierLogin/>} />
+        <Route path="/supplier/registration" element={<SupplierRegistration/>} />
       </Routes>
     </BrowserRouter>
   );
