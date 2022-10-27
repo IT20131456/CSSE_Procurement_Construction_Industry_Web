ee717465a700ced254d049eb126ef4f88b241ed0
IT20128036 <87744181+IT20128036@users.noreply.github.com>
2022-10-28 00:15:00 +0530

    registered supplier view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sample from "../src/components/sample"
 import SupplierLogin from "./components/IT20128036/supplier/UserLogin"
 import SupplierRegistration from"./components/IT20128036/supplier/SupplierRegistration"
+import RequestedSippliers from "./components/IT20128036/staff/RequestedSuppliers";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Sample/>} />
         <Route path="/user/login" element={<SupplierLogin/>} />
         <Route path="/supplier/registration" element={<SupplierRegistration/>} />
+        <Route path="/supplier/req" element={<RequestedSippliers/>} />
       </Routes>
     </BrowserRouter>
   );
