956da38a4c75a2669ccca1a88ba8198b57cc0e0e
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-11-02 22:03:31 +0530

    changes in tenders

--- src/components/IT20125202/ViewTenderDetailsStaff.jsx
+++ src/components/IT20125202/ViewTenderDetailsStaff.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import NavBar from '../IT20128036/supplier/NavBar'
 
 export default function ViewTenderDetailsStaff() {
 
   const navigate = useNavigate()
   const { id } = useParams()
   const [tender, setTender] = React.useState({
+    site: '',
     siteManagerID: '',
     siteManagerName: '',
     items: {},
@@ -21,6 +23,7 @@ export default function ViewTenderDetailsStaff() {
     axios.get(`http://localhost:5000/tender/get/${id}`).then((res) => {
       const receivedTender = res.data.existingTender;
       setTender({
+        site: receivedTender.site,
         siteManagerID: receivedTender.siteManagerID,
         siteManagerName: receivedTender.siteManagerName,
         items: receivedTender.items,
@@ -41,120 +44,136 @@ export default function ViewTenderDetailsStaff() {
 
   return (
     <div>
+      <NavBar />
       {/*<div className='jumbotron' style={{ paddingLeft: '50px', paddingRight: '50px', paddingBottom: '50px', paddingTop: '10px', backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', margin: '0px 0px 0px 0px', overflowY: 'scroll', height: '100vh', borderTop: '5px solid black' }}>*/}
-      <div className='jumbotron' style={{ background: 'white', minHeight: '100vh', padding: '30px 30px 30px 30px' }}>
-        <br />
-        <h1 style={{ textAlign: "center" }}> Tender Details </h1>
-        <br />
-        <hr/>
-        <br/>
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Site Manager ID</h6>
+      <div className="container text-center my-2">
+        <div className='jumbotron' style={{ background: 'white', minHeight: '100vh', padding: '30px 30px 30px 30px', textAlign: 'left' }}>
+          <br />
+          <h1 style={{ textAlign: "center" }}> Tender Details </h1>
+          <br />
+          <hr />
+          <br />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Site </h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.site}</p>
+            </div>
           </div>
-          <div className="col-md-6">
-            <p>{tender.siteManagerID}</p>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Site Manager ID</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.siteManagerID}</p>
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Site Manager Name</h6>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Site Manager Name</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.siteManagerName}</p>
+            </div>
           </div>
-          <div className="col-md-6">
-            <p>{tender.siteManagerName}</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Items</h6>
-          </div>
-          <div className="col-md-6">
-            <p>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Items</h6>
+            </div>
+            <div className="col-md-6">
+              <p>
                 <div className='row'>
-                  <div className='col-md-6'> Item </div>
+                  <div className='col-md-6'> <b>Item</b> </div>
                   <div className='col-md-6'> {tender.items.name} </div>
                 </div>
+                <hr/>
                 <div className='row'>
-                  <div className='col-md-6'> Size </div>
+                  <div className='col-md-6'> <b>Size</b> </div>
                   <div className='col-md-6'> {tender.items.size} </div>
                 </div>
+                <hr/>
                 <div className='row'>
-                  <div className='col-md-6'> Quantity </div>
+                  <div className='col-md-6'> <b>Quantity</b> </div>
                   <div className='col-md-6'> {tender.items.quantity} </div>
                 </div>
+                <hr/>
                 <div className='row'>
-                  <div className='col-md-6'> Order Status </div>
+                  <div className='col-md-6'> <b>Order Status</b> </div>
                   <div className='col-md-6'> {tender.items.orderStatus} </div>
                 </div>
+                <hr/>
                 <div className='row'>
-                  <div className='col-md-6'> Received Amount </div>
+                  <div className='col-md-6'> <b>Received Amount</b> </div>
                   <div className='col-md-6'> {tender.items.receivedAmount} </div>
                 </div>
               </p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Status</h6>
-          </div>
-          <div className="col-md-6">
-            <p>{tender.status}</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Expected Budget</h6>
-          </div>
-          <div className="col-md-6">
-            <p>{tender.expectedBudget}</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Accepted Supplier</h6>
-          </div>
-          <div className="col-md-6">
-            <p>{tender.acceptedSupplier}</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Actual Amount</h6>
-          </div>
-          <div className="col-md-6">
-            <p>{tender.actualAmount}</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-md-6">
-            <h6> Created Date</h6>
-          </div>
-          <div className="col-md-6">
-            <p>{tender.createdDate}</p>
-          </div>
-        </div>
-        <hr />
-        <br />
-
-        {tender.status === 'Need approval' && (
-          <div>
-            <button className="btn btn-primary" onClick={() => onApprove()}>Approve</button>
-            <button className="btn btn-primary" onClick={() => onReject()}>Reject</button>
-          </div>
-        )}
-
-        {tender.status !== 'Need approval' && (
-          <div>
-            <button className="btn btn-primary" onClick={() => navigate(`/staff/tenders`)}>Back to orders</button>
             </div>
-        )}
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Status</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.status}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Expected Budget</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.expectedBudget}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Accepted Supplier</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.acceptedSupplier}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Actual Amount</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.actualAmount}</p>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <h6> Created Date</h6>
+            </div>
+            <div className="col-md-6">
+              <p>{tender.createdDate}</p>
+            </div>
+          </div>
+          <hr />
+          <br />
+
+          {tender.status === 'Need approval' && (
+            <div>
+              <button className="btn btn-primary" onClick={() => onApprove()}>Approve</button>
+              <button className="btn btn-primary" onClick={() => onReject()}>Reject</button>
+            </div>
+          )}
+
+          {tender.status !== 'Need approval' && (
+            <div>
+              <button className="btn btn-primary" onClick={() => navigate(`/staff/tenders`)}>Back to orders</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
