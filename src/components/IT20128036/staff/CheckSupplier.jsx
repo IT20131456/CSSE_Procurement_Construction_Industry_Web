491ea1f1048e23c1451dc935770b215aa4ffb7b4
IT20131456 <it20131456@my.sliit.lk>
2022-11-11 21:53:34 +0530

    Update files

--- src/components/IT20128036/staff/CheckSupplier.jsx
+++ src/components/IT20128036/staff/CheckSupplier.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import jwt_decode from "jwt-decode";
